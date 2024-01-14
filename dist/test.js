6bde8337f4a8eb6eb1a6e5cb552fe8fea45c714f
Jorge Gomez <jorge.electron@gmail.com>
2024-01-13 23:29:39 -0800

    adds a minimum tsconfig.json and a test function

---
+++ dist/test.js
@@ -0,0 +1,9 @@
+"use strict";
+function foo(bar) {
+    return 'Hello, ' + bar;
+}
+let noun = 'world';
+// when we change the value of noun to a number (like 123), 
+// the below line will throw a compilation error:
+// "Argument of type 'number' is not assignable to parameter of type 'string'.ts(2345)"
+console.log(foo(noun));
